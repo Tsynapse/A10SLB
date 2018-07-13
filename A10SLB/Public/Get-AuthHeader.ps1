8ce324362e55a74d1dfb1f52da24d15276d992e5
Jason <bochi28@gmail.com>
2018-07-13 11:00:45 -0400

    Added Notes to description

--- A10SLB/Public/Get-AuthHeader.ps1
+++ A10SLB/Public/Get-AuthHeader.ps1
@@ -2,7 +2,7 @@
 .Synopsis
    Get A10 Auth Header.
 .DESCRIPTION
-   Retrieves the Authentication Header from the A10 for Use in other Functions.
+   Retrieves the Authentication Header from the A10 for Use in other Functions.  This needs to be done with a local A10 account like Admin. The fucntion should be called witht he output going into a variable.
 .EXAMPLE
    $auth = Get-A10Authheader -a10 x.x.x.x
 #>
